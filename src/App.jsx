2230145cd7d0c76ecf9c3dbad0451b3f3dfc28f4
Iftee07 <ifteeiftekhar71@gmail.com>
2024-12-07 02:13:51 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-6xl text-center">Harro everynyan</h1>
+      <button className="btn">Nyan</button>
+    </>
+  );
+}
+
+export default App;
